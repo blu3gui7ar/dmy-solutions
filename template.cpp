f910f4e97023aae801c528121e8d3e91ccd40d88
blu3gui7ar <blu3gui7ar@gmail.com>
2025-06-28 08:23:17 +0800

    redirect support

--- template.cpp
+++ template.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MOD = 998244353;
-int main () {
+int main (int argc, char const *argv[]) {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    // solve the io redirect problem for vscode
+    if (argc > 1){
+        if (argv[1][0] == '<') {
+            freopen(argv[2], "r", stdin);
+        } else {
+            freopen(argv[1], "r", stdin);
+        }
+    }
+
     return 0;
 }
