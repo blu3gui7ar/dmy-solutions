e853c1a949e21231ae87d9058479aed3a7655163
blu3gui7ar <blu3gui7ar@gmail.com>
2025-07-17 14:41:17 +0800

    5 ac

--- bs.daimayuan.top/R1/5.cpp
+++ bs.daimayuan.top/R1/5.cpp
@@ -17,28 +17,29 @@ int main (int argc, char const *argv[]) {
     cin >> n >> k >> x;
     
     vector<int> a(n);
+    vector<ll> prefixCnt(n+1, 0); //小于x的个数前缀和
     for (int i = 0; i < n; i++) {
         cin >> a[i];
+        prefixCnt[i+1] = prefixCnt[i] + (a[i] < x ? 1 : 0);
     }
     
+    // 如果子区间中第k小的数大于等于x，我们称这个子区间是“过分的”。
+    // 等价于子区间中小于x的数的个数小于k
     ll count = 0;
-    // 枚举左端点
     for (int l = 0; l < n; l++) {
-        // 如果子区间中第k小的数大于等于x，我们称这个子区间是“过分的”。
-        // 等价于子区间中小于x的数的个数小于k
-        int lessThanX = 0;
-        // 枚举右端点
-        for (int r = l; r < n; r++) {
-            if (a[r] < x) {
-                lessThanX++;
-            }
-            if (lessThanX < k) {
-                // 子区间是过分的
-                count++;
+        int left = l, right = n - 1;
+        int rs = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (prefixCnt[mid + 1] - prefixCnt[l] < k) {
+                rs = mid;
+                left = mid + 1;
             } else {
-                // 后续子区间都不是过分的，提前跳出
-                break;
+                right = mid - 1;
             }
+        }
+        if (rs != -1) {
+            count += (rs - l + 1);
         }
     }
     
