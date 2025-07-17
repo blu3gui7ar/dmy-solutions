c4141293893e340ac9703e9086f863a1b7c96d48
blu3gui7ar <blu3gui7ar@gmail.com>
2025-07-17 09:17:06 +0800

    5 tle

--- bs.daimayuan.top/R1/5.cpp
+++ bs.daimayuan.top/R1/5.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+typedef long long ll;
+const int MOD = 998244353;
+int main (int argc, char const *argv[]) {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    // solve the io redirect problem for vscode
+    if (argc > 1){
+        if (argv[1][0] == '<') {
+            freopen(argv[2], "r", stdin);
+        } else {
+            freopen(argv[1], "r", stdin);
+        }
+    }
     int n, k, x;
     cin >> n >> k >> x;
     
@@ -13,30 +21,23 @@ int main() {
         cin >> a[i];
     }
     
-    long long count = 0;
-    
-    // For each left endpoint
+    ll count = 0;
+    // 枚举左端点
     for (int l = 0; l < n; l++) {
-        multiset<int> window;
-        
-        // Extend right endpoint
+        // 如果子区间中第k小的数大于等于x，我们称这个子区间是“过分的”。
+        // 等价于子区间中小于x的数的个数小于k
+        int lessThanX = 0;
+        // 枚举右端点
         for (int r = l; r < n; r++) {
-            window.insert(a[r]);
-            int len = r - l + 1;
-            
-            if (len < k) {
-                // Length < k, automatically excessive
+            if (a[r] < x) {
+                lessThanX++;
+            }
+            if (lessThanX < k) {
+                // 子区间是过分的
                 count++;
             } else {
-                // Check if k-th smallest >= x
-                auto it = next(window.begin(), k - 1);
-                if (*it >= x) {
-                    count++;
-                } else {
-                    // Once not excessive, all longer subarrays with same l won't be excessive
-                    // But we still need to check if they become excessive again
-                    // So we can't break here - this optimization doesn't work!
-                }
+                // 后续子区间都不是过分的，提前跳出
+                break;
             }
         }
     }
